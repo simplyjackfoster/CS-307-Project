9c21ce0cb09982ba2bc937cde542867638fb23ee
Max Finder <maxafinder@gmail.com>
2021-10-03 00:55:21 -0400

    Added stack navigator in App.js and created screens directory which contains the questionnaire screen

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { startClock } from 'react-native-reanimated';
+import questionnaire from "./screens/questionnaire";
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="UniRoom" component={questionnaire}/>
+      </Stack.Navigator>
+    </NavigationContainer>    
   );
+
 }
 
+
+// Create styles
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
