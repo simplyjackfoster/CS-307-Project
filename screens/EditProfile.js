c3471de15bb7680696a64edc6a4741d17ea88e54
Max Finder <maxafinder@gmail.com>
2021-10-03 13:39:21 -0400

    Add skeleton code for Feed and Edit Profile screen

---
+++ screens/EditProfile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import Colors from "../constants/Colors";
+
+
+/*
+ * This is the screen where the user can edit their profile.
+ */
+export default () => {
+
+	return (
+		
+		<View>
+			<Text>This is the Edit Profile Screen</Text>
+		</View>
+
+	);
+
+}
+
+
+// styles
+const styles = StyleSheet.create({
+
+	container: {
+
+	},
+
+});
---
+++ screens/Feed.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import Colors from "../constants/Colors";
+
+
+/*
+ * This is the screen where the user can swipe on other
+ * users profiles.
+ */
+export default () => {
+
+	return (
+		
+		<View>
+			<Text>This is the Feed Screen</Text>
+		</View>
+
+	);
+
+}
+
+
+// styles
+const styles = StyleSheet.create({
+
+	container: {
+
+	},
+
+});
