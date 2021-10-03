a00b6537c00bec84c27efc415d1b273d5b0f0587
Max Finder <maxafinder@gmail.com>
2021-10-03 02:45:28 -0400

    Modified App.js

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { startClock } from 'react-native-reanimated';
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
 import Questionnaire from "./screens/Questionnaire";
+import Profile from "./screens/Profile";
 
 const Stack = createStackNavigator();
 
@@ -14,7 +17,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="UniRoom" component={Questionnaire}/>
+        <Stack.Screen name="UniRoom" component={Login}/>
       </Stack.Navigator>
     </NavigationContainer>    
   );
