4080b5220e2612a34bd65748171a299a818a2d20
Max Finder <maxafinder@gmail.com>
2021-10-03 03:16:52 -0400

    Imported Colors from Colors.js to files

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Colors from "./constants/Colors";
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { startClock } from 'react-native-reanimated';
