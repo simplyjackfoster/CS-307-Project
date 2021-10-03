9ba522b6eac898961042b8b32fbad09bf4e6ef97
Max Finder <maxafinder@gmail.com>
2021-10-03 15:20:36 -0400

    Modify headers for the navigation screens

--- App.js
+++ App.js
@@ -3,32 +3,43 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import Colors from "./constants/Colors";
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import Questionnaire from "./screens/Questionnaire";
-import Home from "./screens/Home";
 import Feed from "./screens/Feed";
 import Messages from "./screens/Messages";
 import Profile from "./screens/Profile";
 import EditProfile from "./screens/EditProfile";
+import Settings from "./screens/Settings";
 
 
 const AuthStack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+
+
+const Home = () => (
+  <Tabs.Navigator>
+    <Tabs.Screen name="Profile" component={Profile} options={{ headerShown: false}}></Tabs.Screen>
+    <Tabs.Screen name="Feed" component={Feed} options={{ headerShown: false}}></Tabs.Screen>
+    <Tabs.Screen name="Messages" component={Messages} options={{ headerShown: false}}></Tabs.Screen>
+  </Tabs.Navigator>
+);
+
+
 
 
 export default function App() {
   return (
     <NavigationContainer>
-
-      <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={Home}></Tabs.Screen>
-        <Tabs.Screen name="Feed" component={Feed}></Tabs.Screen>
-        <Tabs.Screen name="Messages" component={Messages}></Tabs.Screen>
-      </Tabs.Navigator>
-        
+      <Drawer.Navigator>
+        <Drawer.Screen name="UniRoom" component={Home}/>
+        <Drawer.Screen name="Settings" component={Settings}/>
+      </Drawer.Navigator>        
     </NavigationContainer>
   );
 }
