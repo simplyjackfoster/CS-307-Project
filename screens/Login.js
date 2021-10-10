b035667cfb9f0f2715db83a28d15e6218ae7145e
Torin Hylan <thylan@purdue.edu>
2021-10-10 15:34:48 -0400

    Added text inputs

--- screens/Login.js
+++ screens/Login.js
@@ -4,19 +4,14 @@ import {
 	Text, 
 	View, 
 	Button,
-	Image
+	Image,
+	TextInput,
+	SafeAreaView,
 } from 'react-native';
 
 import { AuthContext } from "../context";
 import Colors from "../constants/Colors";
 import { render } from 'react-dom';
-
-
-
-
-
-
-
 
 
 
@@ -29,37 +24,59 @@ export default ({ navigation }) => {
 
 	const { userToken, setUserToken } = React.useContext(AuthContext);
 
-	// render() {
+	/* Functions to handle text input changes */
+	const [email, onChangeEmail] = React.useState("email");
+	const [password, onChangePassword] = React.useState("password");
 
-		return (
-			// If the user clicks "Log In", then set userToken to a non-null value.
-			<View style={styles.container}>
-				<Image
-					style={styles.logo}
-					source={require('../images/logo.png')}
+
+
+	return (
+		// If the user clicks "Log In", then set userToken to a non-null value.
+		<View style={styles.container}>
+
+			{/* UniRoom logo */}
+			<Image
+				style={styles.logo}
+				source={require('../images/logo.png')}
+			/>
+
+			{/* Log In (text) */}
+			<Text style={styles.login}>Log In</Text>
+			
+			{/* Email (text), email (field) */}
+			<Text style={styles.email}>Email</Text>
+			<SafeAreaView>
+				<TextInput
+				style={styles.emailInput}
+				onChangeText={onChangeEmail}
+				placeholder={email}
 				/>
-	
-				{/* UniRoom logo */}
-	
-				{/* Log In (text) */}
-				<Text>This is the Log In Screen</Text>
-				
-				{/* Email (text), email (field) */}
-				
-				{/* Password (text), password (field, with black dots) */}
-				
-				{/* Log In (button) */}
-				<Button title="Log In" onPress={() => setUserToken('asdf')}></Button>
-	
-				{/* New to UniRoom? (text), Sign Up (button) */}
-				<Text>New to UniRoom?</Text>
-				<Button title="Sign Up" onPress={() => navigation.push("Signup")}></Button>
-	
-	
-			</View>
-		);
+			</SafeAreaView>
 
-	// }
+
+			
+			{/* Password (text), password (field, with black dots) */}
+			<Text style={styles.password}>Password</Text>
+			<SafeAreaView>
+				<TextInput
+				style={styles.passwordInput}
+				onChangeText={onChangePassword}
+				placeholder={password}
+				/>
+			</SafeAreaView>
+
+			
+			{/* Log In (button) */}
+			<Button title="Log In" onPress={() => setUserToken('asdf')}></Button>
+
+			{/* New to UniRoom? (text), Sign Up (button) */}
+			<Text>New to UniRoom?</Text>
+			<Button title="Sign Up" onPress={() => navigation.push("Signup")}></Button>
+
+
+		</View>
+	);
+
 
 }
 
@@ -76,11 +93,43 @@ const styles = StyleSheet.create({
   },
 
   logo: {
-	height: 200,
-	width: 250,
+	height: 250,
+	width: 350,
 	margin: 25,
-	borderRadius: 50
+	borderRadius: 50,
+  },
+
+  login: {
+	fontSize: 35,
+	textAlign: 'left',
 	
   },
+
+  email: {
+	fontSize: 20,
+  },
+
+  emailInput: {
+	height: 40,
+	width: 200,
+	margin: 10,
+	padding: 10,
+	borderWidth: 1,
+	borderRadius: 10,
+  },
+
+  password: {
+	fontSize: 20,
+  },
+
+  passwordInput: {
+	height: 40,
+	width: 200,
+	margin: 10,
+	padding: 10,
+	borderWidth: 1,
+	borderRadius: 10,
+  },
+
 
 });
