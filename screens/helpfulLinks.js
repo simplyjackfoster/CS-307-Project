2b46e7869227a08be4feb3b4b5fea502a41984a0
Jack Foster <jackfoster246@gmail.com>
2021-10-10 15:25:57 -0400

    Update helpfulLinks.js

--- screens/helpfulLinks.js
+++ screens/helpfulLinks.js
@@ -1,1 +1,9 @@
-
+import { Linking } from 'react-native';
+<Text style={{color: 'blue'}}
+      onPress={() => Linking.openURL('https://www.housing.purdue.edu/')}>
+  Purdue Housing Home Page
+</Text>
+<Text style={{color: 'blue'}}
+      onPress={() => Linking.openURL('https://www.housing.purdue.edu/my-housing/index.html')}>
+  Purdue Housing Options
+</Text>
