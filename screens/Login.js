455a4fa79b1cc87aed425a71071d367878a72f1a
Torin Hylan <thylan@purdue.edu>
2021-10-10 16:07:58 -0400

    created basic login ui

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {Component} from 'react';
+import { 
+	StyleSheet, 
+	Text, 
+	View, 
+	Button,
+	Image,
+	TextInput,
+	SafeAreaView,
+} from 'react-native';
 
 import { AuthContext } from "../context";
 import Colors from "../constants/Colors";
+import { render } from 'react-dom';
+
 
 
 /*
@@ -14,16 +24,64 @@ export default ({ navigation }) => {
 
 	const { userToken, setUserToken } = React.useContext(AuthContext);
 
+	/* Functions to handle text input changes */
+	const [email, onChangeEmail] = React.useState("email");
+	const [password, onChangePassword] = React.useState("password");
+
+
+
 	return (
 		// If the user clicks "Log In", then set userToken to a non-null value.
 		<View style={styles.container}>
-			<Text>This is the Log In Screen</Text>
+
+			{/* UniRoom logo */}
+			<Image
+				style={styles.logo}
+				source={require('../images/logo.png')}
+			/>
+
+			{/* Log In (text) */}
+			<Text style={styles.login}>Log In</Text>
+			
+			{/* Email (text), email (field) */}
+			<Text style={styles.email}>Email</Text>
+			<SafeAreaView>
+				<TextInput
+				style={styles.emailInput}
+				onChangeText={onChangeEmail}
+				placeholder={email}
+				/>
+			</SafeAreaView>
+
+
+			
+			{/* Password (text), password (field, with black dots) */}
+			<Text style={styles.password}>Password</Text>
+			<SafeAreaView>
+				<TextInput
+				style={styles.passwordInput}
+				onChangeText={onChangePassword}
+				placeholder={password}
+				/>
+			</SafeAreaView>
+
+			
+			{/* Log In (button) */}
 			<Button title="Log In" onPress={() => setUserToken('asdf')}></Button>
+
+			{/* New to UniRoom? (text), Sign Up (button) */}
+			<Text>New to UniRoom?</Text>
 			<Button title="Sign Up" onPress={() => navigation.push("Signup")}></Button>
+
+			{/* Forgot password (button) */}
+			
+
+
 		</View>
 	);
-}
 
+
+}
 
 
 
@@ -34,7 +92,47 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+
+  logo: {
+	height: 250,
+	width: 350,
+	margin: 25,
+	borderRadius: 50,
+  },
+
+  login: {
+	fontSize: 35,
+	textAlign: 'left',
+	
+  },
+
+  email: {
+	fontSize: 20,
+  },
+
+  emailInput: {
+	height: 40,
+	width: 200,
+	margin: 10,
+	padding: 10,
+	borderWidth: 1,
+	borderRadius: 10,
+  },
+
+  password: {
+	fontSize: 20,
+  },
+
+  passwordInput: {
+	height: 40,
+	width: 200,
+	margin: 10,
+	padding: 10,
+	borderWidth: 1,
+	borderRadius: 10,
+  },
+
 
 });
