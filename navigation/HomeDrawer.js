3bc2c19c48bb3399d8e41d26b9480386168fa374
Max Finder <maxafinder@gmail.com>
2021-10-10 16:42:47 -0500

    Moved navigation logic to "./navigation" folder

---
+++ navigation/HomeDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+import HomeTabs from './HomeTabs';
+import Account from '../screens/Account';
+import Settings from '../screens/Settings';
+import CodeOfConduct from '../screens/CodeOfConduct';
+import PrivacyPolicy from '../screens/PrivacyPolicy';
+import FAQ from '../screens/FAQ';
+import Help from '../screens/Help';
+
+const Drawer = createDrawerNavigator();
+
+// Drawer on the left that includes "HomeTabs" and "Settings" 
+const HomeDrawer = () => {
+	return (
+		<Drawer.Navigator>
+			<Drawer.Screen name="Home" component={HomeTabs} options={{ headerTitle: "UniRoom" }}/>
+			<Drawer.Screen name="Account" component={Account}/>
+			<Drawer.Screen name="Settings" component={Settings}/>
+			<Drawer.Screen name="CodeOfConduct" component={CodeOfConduct}
+			options={{ title: "Code of Conduct"}}/>
+			<Drawer.Screen name="PrivacyPolicy" component={PrivacyPolicy}
+			options={{ title: "Privacy Policy"}}/>
+			<Drawer.Screen name="FAQ" component={FAQ}/>
+			<Drawer.Screen name="Help" component={Help}/>
+		</Drawer.Navigator>
+	);
+}
+
+export default HomeDrawer;
