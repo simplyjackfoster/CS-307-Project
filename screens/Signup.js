093c289b3904753415d7505bba2025cf7a992a42
Torin Hylan <thylan@purdue.edu>
2021-10-12 14:48:05 -0400

    Added function to validate all inputs except birthday

--- screens/Signup.js
+++ screens/Signup.js
@@ -7,7 +7,8 @@ import {
   Button,
   SafeAreaView,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert
 } from 'react-native';
 
 import { AuthContext } from "../context";
@@ -24,12 +25,12 @@ export default ( {navigation} ) => {
 
 
   /* Functions to handle text input changes */
-  const [name, onChangeName] = React.useState("");
-  const [email, onChangeEmail] = React.useState("");
-  const [phone, onChangePhone] = React.useState("");
-  const [birthday, onChangeBirthday] = React.useState("");
-  const [password, onChangePassword] = React.useState("");
-  const [confirmPassword, onChangeConfirmPassword] = React.useState("");
+  const [name, onChangeName] = React.useState(null);
+  const [email, onChangeEmail] = React.useState(null);
+  const [phone, onChangePhone] = React.useState(null);
+  const [birthday, onChangeBirthday] = React.useState(null);
+  const [password, onChangePassword] = React.useState(null);
+  const [confirmPassword, onChangeConfirmPassword] = React.useState(null);
 
 
 
@@ -44,7 +45,8 @@ export default ( {navigation} ) => {
 
     // navigate the user to the questionnaire screen
     navigation.push("Questionnaire");
-  }
+  } // goToQuest
+
 
 
   /*
@@ -53,11 +55,18 @@ export default ( {navigation} ) => {
    * alphabetic characters, apostrophes, and hyphens and false otherwise 
    */
   const isValidName = () => {
-    //check if there are any characters besides uppercase and lowercase letters, apostrophes, and hyphens
+    console.log("Validating the name: " + name);
+
+    if (!name) {
+      Alert.alert("Error", "Name field is empty, please try again.", 
+				[{ text: "Ok" }]);
+      return false;
+    }
+
+    // iterate through the characters in the name
     for (const c of name) {
 
-      // 8217 and 8216 are the acsii values for apostrophes in iOS
-
+      // 8216 and 8217 are the acsii values for apostrophes in iOS
       if ((c.charCodeAt(0) == 8217) || (c.charCodeAt(0) == 8216) || (c === '-') || 
           (c === ' ') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
         // valid character
@@ -65,7 +74,10 @@ export default ( {navigation} ) => {
       }
       else {
         // invalid character
-        console.log(c.charCodeAt(0) + " is NOT a valid character");
+        Alert.alert("Error", "Name field contains invalid character: '" + c + 
+          "', please try again.", [{ text: "Ok" }]);
+
+        console.log(c + " is NOT a valid character");
         return false;
       }
     }
@@ -82,17 +94,31 @@ export default ( {navigation} ) => {
    * returns true if those conditions are met, false if not
    */
   const isValidEmail = () => {
+    console.log("Validating the email: " + email);
+
+    if (!email) {
+      Alert.alert("Error", "Email field is empty, please try again.", 
+				[{ text: "Ok" }]);
+      return false;
+    }
+
     const indexOfAt = email.indexOf('@');
 
     // check for @ character
     if (indexOfAt == -1) {
-      console.log("No @ character");
+      Alert.alert("Error", "Email is missing '@' character, please try again.", 
+				[{ text: "Ok" }]);
+
+      // console.log("No @ character");
       return false;
     } 
     
     // check for purdue email
     if (email.substring(indexOfAt) !== "@purdue.edu") {
-      console.log("Not a purdue email");
+      Alert.alert("Error", "Email is not a purdue email, please try again.", 
+				[{ text: "Ok" }]);
+
+      // console.log("Not a purdue email");
       return false;
     }
 
@@ -101,18 +127,24 @@ export default ( {navigation} ) => {
     
     // check for empty username
     if (username.length === 0) {
-      console.log("Empty username");
+      Alert.alert("Error", "Email is missing username, please try again.", 
+				[{ text: "Ok" }]);
+
+      // console.log("Empty username");
       return false;
     }
 
     // iterate through the username characters
     for (const c of username) {
       if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
-        console.log(c + " is a valid character");
+        // console.log(c + " is a valid character");
       }
       else {
         // invalid character
-        console.log(c + " is NOT a valid character");
+        Alert.alert("Error", "Email contains invalid character '" + c + 
+          "' in username, please try again.", [{ text: "Ok" }]);
+
+        // console.log(c + " is NOT a valid character");
         return false;
       }
     }
@@ -122,24 +154,36 @@ export default ( {navigation} ) => {
   } // isValidEmail
 
 
+
   /*
    * isValidPhone()
    * function to make sure the phone number only consists of 10 numbers
    * $$ Issue with interpreting '.' as the same value as numbers $$
    */
   const isValidPhone = () => {
-    console.log(phone);
+    console.log("Validating the phone: " + phone);
     
+    if (!phone) {
+      Alert.alert("Error", "Phone field is empty, please try again.", 
+				[{ text: "Ok" }]);
+      return false;
+    }
 
     if (phone.length !== 10) {
-      console.log("Phone number not 10 characters long");
+      Alert.alert("Error", "Phone number is not 10 digits long, please try again.", 
+				[{ text: "Ok" }]);
+
+      // console.log("Phone number not 10 digits long");
       return false;
     }
 
     for (const c in phone) {
       console.log(c.charCodeAt(0));
       if (c < '0' || c > '9' || c == '.') {
-        console.log("Non-numeric character found");
+        Alert.alert("Error", "Phone number contains non-numeric character, please try again.", 
+				[{ text: "Ok" }]);
+
+        // console.log("Non-numeric character found");
         return false;
       }
     }
@@ -154,7 +198,15 @@ export default ( {navigation} ) => {
    * isValidBirthday()
    *  
    */
-  /* const isValidBirthday = () => {
+/*   const isValidBirthday = () => {
+    console.log("Validating the birthday: " + birthday);
+
+    if (!birthday) {
+      Alert.alert("Error", "Birthday field is empty, please try again.", 
+				[{ text: "Ok" }]);
+      return false;  
+    }
+
     if(birthday.length != 10){
       console.log("Not a valid length for a date")
       return false;
@@ -174,19 +226,70 @@ export default ( {navigation} ) => {
    * and also have the length within the specified range of 8 to 28 
    */
   const isValidPassword = () => {
+    console.log("Validating the password: " + password + 
+                ", confirm password: " + confirmPassword);
+
+    if (!password || !confirmPassword) {
+      Alert.alert("Error", "Password and/or Confirm Password field is empty, please try again.", 
+        [{ text: "Ok" }]);
+      return false;
+    }
+
     if (password !== confirmPassword) {
-      console.log("Passwords do not match");
-      //alert the user that passwords do not match
+      Alert.alert("Error", "Passwords do not match, please try again.", 
+				[{ text: "Ok" }]);
+
+      // console.log("Passwords do not match");
       return false;
     }
     if (password.length > 28 || password.length < 8) {
-      console.log("Password is not a valid length");
+      Alert.alert("Error", "Password must be within range 8-28 characters, please try again.", 
+				[{ text: "Ok" }]);
+
+      // console.log("Password is not a valid length");
       return false;
     }
 
     console.log("Password Validated");
     return true;
   } // isValidPassword
+
+
+
+    /*
+   * validateInputs()
+   * function to check all of the input fields and send the appropriate alert
+   * if there is an issue with one of the fields. This function will utilize helper
+   * functions that validate each of the specific fields and send the alert.
+   * If all fields are validated, allow the user to sign up.
+   */
+    const validateInputs = () => {
+      console.log("\n\n...Authenticating signup...");
+  
+      // validate name
+      if (!isValidName()) return;
+  
+      // validate email
+      if (!isValidEmail()) return;
+  
+      // validate phone
+      if (!isValidPhone()) return;
+  
+      // validate birthday
+      // if (!isValidBirthday()) return;
+  
+      // validate passwords
+      if (!isValidPassword()) return;
+  
+      // All fields have valid inputs, so create the account
+  
+  
+      // on successful login, set userToken to a non-null value
+      setUserToken('Arbitrary text');
+  
+      console.log("Successful Signup!")
+  
+    } // validateInputs
 
 
 
@@ -244,7 +347,7 @@ export default ( {navigation} ) => {
 
           
           {/* Password (text), password (field, with black dots) */}
-          <Text style={styles.label}>Password (8 to 28 characters)</Text>
+          <Text style={styles.label}>Password (8-28 characters)</Text>
           <SafeAreaView>
             <TextInput
               style={styles.input}
@@ -284,7 +387,7 @@ export default ( {navigation} ) => {
           <TouchableOpacity
 					style={styles.createButton}
           // check if questionnaire has been completed and run setUserToken
-          onPress={() => setUserToken('asdsf')}
+          onPress={validateInputs}
 				  >
             <Text>Create Account</Text>
 				  </TouchableOpacity>
