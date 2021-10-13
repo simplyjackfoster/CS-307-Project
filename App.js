b5b36885f23ae93747e09b247e5d81b9f64f0426
jwerner51 <werner51@purdue.edu>
2021-10-13 19:02:58 -0400

    //added firebase to this branch

--- App.js
+++ App.js
@@ -9,7 +9,33 @@ import Colors from "./constants/Colors";
 import { AuthContext } from "./context"
 import RootStack from './navigation/RootStack';
 
+// Firebase Integration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue, exists, val, child, get } from "firebase/database";
+import { getFirestore, collection, getDocs, setDoc, docRef, doc } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCVc6_sT83QWcX-TCxDEDtVMHsMRaTy2yY",
+  authDomain: "uniroom-fdcd7.firebaseapp.com",
+  databaseURL: "https://uniroom-fdcd7-default-rtdb.firebaseio.com",
+  projectId: "uniroom-fdcd7",
+  storageBucket: "uniroom-fdcd7.appspot.com",
+  messagingSenderId: "644435940478",
+  appId: "1:644435940478:web:40e3f7aea01972606bb42f",
+  measurementId: "G-KQK1K10WTL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const rtdb = getDatabase(app);
 /*
  * This is the default export for the App.
  */
