5ce3d72386c9d951171c46fbd9cea68120f51660
Max Finder <maxafinder@gmail.com>
2021-10-14 22:10:02 -0400

    Added Color import statement in ResetPassword.js

--- screens/ResetPassword.js
+++ screens/ResetPassword.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { TouchableOpacity, TextInput, StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native';
 
+import Colors from "../constants/Colors";
+
 /*
  * This is the screen where the user resets their password.
  */
