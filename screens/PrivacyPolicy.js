5a482c14d748cecbffd1753a33fa1b764c04ee34
Jack Foster <jackfoster246@gmail.com>
2021-10-15 02:54:41 -0400

    Add Privacy Policy

--- screens/PrivacyPolicy.js
+++ screens/PrivacyPolicy.js
@@ -10,8 +10,18 @@ import Colors from "../constants/Colors";
 export default ({ navigation }) => {
 	return (
 		<View style={styles.container}>
-			<Text>This is the Privacy Policy Screen</Text>
-		</View>
+      <View style={styles.header}>
+			  <Text>Privacy Notice</Text>
+      </View>
+      <Text>Thank you for choosing to be part of our community at UniRoom ("Company," "we," "us," or "our"). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy notice or our practices with regard to your personal information, please contact us at company@email.com.</Text>
+      <View style={styles.header}>
+        <Text>WHAT INFORMATION DO WE COLLECT?</Text>
+		  </View>
+      <View style={styles.container}>
+			  <Text>We collect personal information that you voluntarily provide to us when you register on the App, express an interest in obtaining information about us or our products and Services, when you participate in activities on the App (such as by posting messages in our online forums or entering competitions, contests or giveaways) or otherwise when you contact us.
+</Text>
+      </View>
+    </View>
 	);
 }
 
@@ -22,9 +32,15 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: Colors.white,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    fontSize: 32
+  },
+  body:{
+    fontSize: 18
+  }
 
 });
