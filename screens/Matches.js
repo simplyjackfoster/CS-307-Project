d83cd882af4b3832b01f32c30ce80741ef5ed7a5
Jack Foster <jackfoster246@gmail.com>
2021-11-01 13:49:15 -0400

    Update Matches.js

--- screens/Matches.js
+++ screens/Matches.js
@@ -1,34 +1,20 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
-
+import React from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  Button } from 'react-native';
 import Colors from "../constants/Colors";
+import MatchItem from '../components/MatchItem';
 
 
 /*
  * This is the screen where the user can view their matches.
  */
 export default ( {navigation} ) => {
-  const matches_list = {
-    // Sample data
-    matches: [
-      { item: "John Smith", major: "Computer Science", id: 1 },
-      { item: "Sarah Davis", major: "Data Science", id: 2 },
-    ],
-  };
 	return (
 		<View style={styles.container}>
-      <ScrollView>
-      {matches_list.matches.map((item, index) => (
-            <View key={item.id}>
-              <View style={styles.summary}>
-                <Text style={styles.summaryText}>
-                  {item.item} <Text style={styles.majorText}>
-                      Major: {item.major}</Text>
-                </Text>
-              </View>
-            </View>
-          ))}
-        </ScrollView>
+			<MatchItem> id = {"mfinder"}</MatchItem>
 		</View>
 	);
 }
@@ -43,26 +29,6 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  summary: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
-    padding: 10,
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
-  },
-  summaryText: {
-    fontSize: 18,
-    fontWeight: 'bold'
-  },
-  majorText: {
-    color: Colors.orange,
-    fontWeight: 'normal',
-    fontSize: 12,
   },
 
 });
