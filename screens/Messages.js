e471bb8a6961cb5318c4d298c5084046496a7e9b
simplyjackfoster <jackfoster246@gmail.com>
2021-11-08 23:22:19 -0500

    Messages UI

--- screens/Messages.js
+++ screens/Messages.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { createStackNavigator } from 'react-navigation';
+import Chat from '../components/Chat';
+import { GiftedChat } from 'react-native-gifted-chat';
 import Colors from '../constants/Colors';
+import { getDataFromPath, getInstagramLink } from "../database/readData";
 
 
 /*
  * This is the screen where the user messages other users.
  */
-export default () => {
-	return (
-		<View style={styles.container}>
-			<Text>This is the Messages Screen</Text>
-		</View>
-	);
+export default ({ navigation }) =>{
+  const [messages, setMessages] = useState([]);
+  const profile_picture = getDataFromPath("users/" + "foste205" + "/Profile/Images/profile_picture");
+
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hey do you want to be roommates?',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: profile_picture,
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 }
 
 
